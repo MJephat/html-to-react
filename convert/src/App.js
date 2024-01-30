11c750850d861bea97545c0f557ef2f959478f6b
MJephat <mukundijephat22@gmail.com>
2024-01-30 09:28:16 +0300

    sidebar converted

--- convert/src/App.js
+++ convert/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Sidenavbar from './component/side-navbar';
 import Topnavbar from './component/top-navbar';
 
 function App() {
   return (
     <div className="App">
       <Topnavbar />
+      <Sidenavbar />
       
     </div>
   );
